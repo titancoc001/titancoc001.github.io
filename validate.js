24e49175bf0528d4c8a118fee98032117e9c1823
Oscar Gonzales <titancoc001@gmail.com>
2025-10-16 23:13:25 -0400

    fixed

--- validate.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  var script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/gh/gracehoppercenter/validate@1.0.0/validate.js";
-  script.async = false;
-  document.head.appendChild(script);
-})();
